afc4a8f40d0622563e7d9a699b710b2529ef4dcf
kavishka199808 <it20301590@my.sliit.lk>
2022-07-20 02:51:33 +0530

    funtion with return type

--- CasterKotlin/src/main/kotlin/main2.kt
+++ CasterKotlin/src/main/kotlin/main2.kt
@@ -14,3 +14,35 @@ fun moo(){
     println("Moo!")
     println("Moo Moo Buckaroo!")
 }
+
+
+//---------------------------------------
+fun main3(){
+    var x = sayHi()
+    println(x)
+
+}
+fun sayHi():String{
+    return "Hello World"
+}
+
+//---------------------------------------
+fun main4(){
+    var y =sayHii()
+    println(y)
+
+}
+fun sayHii():Int{
+    return 32
+}
+
+//---------------------------------------
+fun main5(){
+    var y =sayHiii()
+    println(y)
+
+}
+fun sayHiii():String{
+    return 32.toString()
+}
+
