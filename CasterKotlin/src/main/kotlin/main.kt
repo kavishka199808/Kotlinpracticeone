b2f51f99028c164bc42a64aa4ca8b12686bf2dd5
kavishka199808 <it20301590@my.sliit.lk>
2022-07-19 23:13:54 +0530

    creating variables

--- CasterKotlin/src/main/kotlin/main.kt
+++ CasterKotlin/src/main/kotlin/main.kt
@@ -1,3 +1,18 @@
 fun main() {
     println("Hello World")
+
+    var fullName = "Kavishka Dislshan"
+    println(fullName)
+
+    fullName = "Tharidu buddika"
+    println(fullName)
+
+    fullName = ""
+    println(fullName)
+
+    println(fullName.isEmpty())
+
+    var age = 30
+    age = 32
+    println(age)
 }
